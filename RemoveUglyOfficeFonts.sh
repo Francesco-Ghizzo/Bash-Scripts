eb39399411f7cc5f4819ef7752cb3970c5486776
Francesco Ghizzo <Francesco@Francescos-MacBook-Pro.local>
2021-05-03 12:30:40 +0200

    Aggiunta estensione.sh agli eseguibili

---
+++ RemoveUglyOfficeFonts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /Applications/Microsoft\ Excel.app/Contents/Resources/Fonts;
+rm ComicSansMSBold.ttf; 
+rm CurlzMT.ttf; 
+
+cd /Applications/Microsoft\ PowerPoint.app/Contents/Resources/Fonts;
+rm ComicSansMSBold.ttf;
+rm CurlzMT.ttf;
+
+cd /Applications/Microsoft\ Word.app/Contents/Resources/Fonts;
+rm ComicSansMSBold.ttf;
+rm CurlzMT.ttf;
---
+++ RemoveUglySystemFonts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /Library/Fonts;
+rm /Library/Fonts/Comic\ Sans\ MS\ Bold.ttf;
+rm /Library/Fonts/Comic\ Sans\ MS.ttf;
+rm /Library/Fonts/Papyrus.ttc 
