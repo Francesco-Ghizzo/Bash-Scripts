0d83b3952b0d70ed27121115f8bcfb62fe29d98e
Francesco Ghizzo <francescoghizzo@Francescos-MacBook-Pro.local>
2021-05-24 08:21:59 +0200

    fast_diff.sh: aggiunte variabili regexp1 e regexp2

--- fast_diff.sh
+++ fast_diff.sh
@@ -2,8 +2,7 @@
 # script che accetta due path come argomenti e controlla che il numero ed il nome dei files siano uguali in tutte le sottocartelle, 
 # senza preoccuparsi che il contenuto dei files sia effettivamente uguale, ignorando i files che corrispondono a determinnate espressioni regolari.
 
-diff <(find "$1" \! -name ".*" | sed "s|$1\/||g" | sort) <(find "$2" \! -name "Icon?" | sed "s|$2\/||g" | sort) 
+regexp1=.* # Ignora files nascosti, come ad esempio .DS_Store
+regexp2=Icon? # Ignora icone di Google Drive
 
-# sarebbe meglio qualcosa come:
-# regexp1=".*"
-# regexp2="Icon?"
+diff <(find "$1" \! -name "regexp1" | sed "s|$1\/||g" | sort) <(find "$2" \! -name "regexp2" | sed "s|$2\/||g" | sort) 
