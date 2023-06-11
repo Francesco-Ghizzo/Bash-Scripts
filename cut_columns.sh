6cda50d57d70abb4c176fc13ff8edb8fcab0d2cc
Francesco-Ghizzo <61307774+Francesco-Ghizzo@users.noreply.github.com>
2023-06-11 19:06:42 +0200

    Added 'delimiter' variable
    For separators different than comma, just change 'delimiter' variable

--- cut_columns.sh
+++ cut_columns.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This small tool cuts extracts a column interval from a comma separated (.csv) file.
+# For separators different than comma (','), change 'delimiter' variable
+
 # Usage: 
 
 # chmod +x cut_columns.sh 
@@ -9,6 +12,7 @@
 
 # ./cut_columns.sh file.csv 2 3
 
+delimiter=","
 
 filename=$1
 
@@ -20,7 +24,7 @@ cut_columns=""
 
 for (( column=start_column; column<=end_column; column++ ))
 do
-	cut_columns+="<(cut -d\; -f$column < $filename | head) "
+	cut_columns+="<(cut -d$delimiter -f$column < $filename | head) "
 done
 
 full_command="paste -d, "$cut_columns
