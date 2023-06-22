16f27244d7e48e59cc3797b0aee91ac1fbb8f6f3
Francesco-Ghizzo <61307774+Francesco-Ghizzo@users.noreply.github.com>
2023-06-22 19:47:52 -0200

    Add files via upload

--- fast_diff.sh
+++ fast_diff.sh
@@ -2,7 +2,7 @@
 # script che accetta due path come argomenti e controlla che il numero ed il nome dei files siano uguali in tutte le sottocartelle, 
 # senza preoccuparsi che il contenuto dei files sia effettivamente uguale, ignorando i files che corrispondono a determinnate espressioni regolari.
 
-regexp1={.*,.*/} # Ignora files nascosti, come ad esempio .DS_Store e cartelle nascoste
+regexp1=.* # Ignora files nascosti, come ad esempio .DS_Store e cartelle nascoste
 regexp2=Icon? # Ignora icone di Google Drive
 
 diff <(find "$1" \! -name "$regexp1" | sed "s|$1\/||g" | sort) <(find "$2" \! -name "$regexp2" | sed "s|$2\/||g" | sort) 
